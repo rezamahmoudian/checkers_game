0ed8b806d48da0dbaa34d91ea7e91ecdf1fdf0d0
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-06-04 13:14:36 +0330

    crate main function

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pygame
 from checkers.constrant import WIDTH, HEIGHT
 
+FPS = 60
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('checkers')
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        pass
+
+main()
